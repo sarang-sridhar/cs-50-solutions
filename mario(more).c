7305e7d788f4077bdd90ff712fa4cbe52555afc9
saremaster <74752543+saremaster@users.noreply.github.com>
2020-11-23 13:44:53 +0530

    Create mario(more).c

---
+++ mario(more).c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+void space(int p)
+{
+for(int k=0;k<p;k++)
+printf(" ");
+}
+
+int main(void)
+{
+int height;
+do {
+printf("height: ");
+scanf("%d",&height);
+}while((height<1)||(height>8));
+
+int j; 
+
+
+for(int i=0;i<height;i++)
+{
+space(n-i-1);
+
+for(j=0;j<=i;j++)
+{
+printf("#");
+}
+
+space(2);
+
+for(j=0;j<=i;j++)
+{
+printf("#");
+}
+
+printf("\n");
+
+}
+}
+
+
